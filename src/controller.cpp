761686649146636dbda6bbd7978721d576ac8346
Andrei Maximov <andrei.maximov@gmail.com>
2015-11-23 00:02:20 -0800

    Implement command parsing.

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,5 +1,40 @@
 #include <string>
 #include "controller.h"
 
+const std::string Controller::ADD_CMD = "ADD";
+
+const std::string Controller::DEL_CMD = "DEL";
+
+const std::string Controller::QUERY_CMD = "QUERY";
+
+const std::string Controller::WQUERY_CMD = "WQUERY";
+
 void Controller::call(std::string* command) {
+    COMMAND cmd = this->getCommand(command);
+    switch (cmd) {
+        case COMMAND::ADD: break;
+        case COMMAND::DEL: break;
+        case COMMAND::QUERY: break;
+        case COMMAND::WQUERY: break;
+    }
+}
+
+COMMAND Controller::getCommand(std::string* command) {
+    size_t i = command->find_first_of(' ');
+    if (i == std::string::npos) {
+        throw std::invalid_argument("Could not parse command!");
+    }
+
+    std::string cmd = command->substr(0, i + 1);
+    if (cmd.compare(Controller::ADD_CMD) != 0) {
+        return COMMAND::ADD;
+    } else if (cmd.compare(Controller::DEL_CMD) != 0) {
+        return COMMAND::DEL;
+    } else if (cmd.compare(Controller::QUERY_CMD) != 0) {
+        return COMMAND::QUERY;
+    } else if (cmd.compare(Controller::WQUERY_CMD) != 0) {
+        return COMMAND::WQUERY;
+    }
+
+    throw std::invalid_argument("Invalid command!");
 }
--- src/controller.h
+++ src/controller.h
@@ -3,8 +3,23 @@
 #ifndef SRC_CONTROLLER_H_
 #define SRC_CONTROLLER_H_
 
+enum COMMAND {
+    ADD, DEL, QUERY, WQUERY
+};
+
 class Controller {
+ private:
+    virtual COMMAND getCommand(std::string* command);
+
  public:
+    static const std::string ADD_CMD;
+
+    static const std::string DEL_CMD;
+
+    static const std::string QUERY_CMD;
+
+    static const std::string WQUERY_CMD;
+
     virtual void call(std::string* command);
 };
 
