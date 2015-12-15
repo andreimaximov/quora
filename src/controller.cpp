ad1243711037ee80197ee0cce64e5647b123b203
Andrei Maximov <andrei.maximov@gmail.com>
2015-12-14 23:50:15 -0800

    Output fix.

--- src/controller.cpp
+++ src/controller.cpp
@@ -65,8 +65,10 @@ void Controller::del(const std::string &command) {
 void Controller::query(const std::string &command) {
     Query q = this->queryParser->parse(command);
     std::vector<std::string> results = this->memoryService->query(q);
+    std::string space = "";
     for (std::string &id : results) {
-        this->out << id << " ";
+        this->out << space << id;
+        space = " ";
     }
     this->out << std::endl;
 }
