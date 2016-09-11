cdc098c3656ffeb7855138b774b74b17cdfae470
Andrei Maximov <andrei.maximov@gmail.com>
2016-09-10 23:05:07 -0700

    Setup input parsing

--- __main__.py
+++ __main__.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
+import sys
+
+
+def read(fn):
+    return [fn(x) for x in sys.stdin.readline().split(' ')]
+
 
 def main():
-    pass
-
+    (N, K) = read(int)
+    upvotes = read(int)
 
 if __name__ == "__main__":
     main()
