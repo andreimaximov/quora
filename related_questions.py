bc22b188d3f56648f48890bdae9f7b7fb36ef173
Andrei Maximov <andrei.maximov@gmail.com>
2016-09-11 15:56:05 -0700

    Implement input parsing and building graph

--- related_questions.py
+++ related_questions.py
@@ -1,8 +1,48 @@
 #!/usr/bin/env python
 
+import sys
+
+
+class Question(object):
+    def __init__(self, time):
+        self._time = time
+        self._related = []
+
+    def time(self):
+        return self._time
+
+    def related(self, question):
+        self._related.append(question)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return 'Question (time = %d)' % self._time
+
+
+def read(fn):
+        return [fn(x) for x in sys.stdin.readline().split()]
+
+
+def build_graph(N, T):
+    graph = [Question(T[i]) for i in range(0, N)]
+
+    for i in range(0, N - 1):
+        (A, B) = read(int)
+        A -= 1
+        B -= 1
+        graph[A].related(B)
+        graph[B].related(A)
+
+    return graph
+
 
 def main():
-    pass
+    N = int(sys.stdin.readline())
+    T = read(int)
+    graph = build_graph(N, T)
+    print(graph)
 
 if __name__ == '__main__':
     main()
